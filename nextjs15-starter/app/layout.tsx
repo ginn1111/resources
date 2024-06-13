4bd1262b223e2016996a607ceda2c46a53c403e4
ginn1111 <vanthuanjw@gmail.com>
2024-06-13 16:50:47 +0700

    chore: setup shadcn ui

--- nextjs15-starter/app/layout.tsx
+++ nextjs15-starter/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Exo } from "next/font/google";
 import QueryProvider from "@/providers/query-provider";
+import { cn } from "@/libs/utils";
+import "./globals.css";
 
-const exo = Exo({ subsets: ["vietnamese"] });
+const exo = Exo({ subsets: ["vietnamese"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "NextJS v15 Starter",
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={exo.className}>
+      <body
+        className={cn(
+          "bg-background font-sans antialiased min-h-screen",
+          exo.variable,
+        )}
+      >
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
--- nextjs15-starter/app/page.tsx
+++ nextjs15-starter/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p>Home</p>;
+  return (
+    <>
+      <p>Home</p>
+      <Button>Click me</Button>
+    </>
+  );
 }
