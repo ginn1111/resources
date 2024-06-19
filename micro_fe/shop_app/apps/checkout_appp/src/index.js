f3e54338b243def0212d63b909f3a2f2008ab9ef
ginn1111 <vanthuanjw@gmail.com>
2024-06-19 11:53:07 +0700

    micro_fe/shop_app: config vue loader for webpack

--- micro_fe/shop_app/apps/checkout_appp/src/index.js
+++ micro_fe/shop_app/apps/checkout_appp/src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello World!");
+import '../public/assets/style.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#root-checkout-app');
--- micro_fe/shop_app/apps/checkout_appp/webpack.config.js
+++ micro_fe/shop_app/apps/checkout_appp/webpack.config.js
@@ -1,74 +1,62 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const mfeConfig = require("@repo/configs/mfe.config");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const mfeConfig = require('@repo/configs/mfe.config');
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV == 'production';
 
 const stylesHandler = isProduction
   ? MiniCssExtractPlugin.loader
-  : "style-loader";
+  : 'style-loader';
 
 const config = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     port: mfeConfig.checkout_app.port,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: './public/index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new VueLoaderPlugin(),
   ],
   resolve: {
     extensions: [
-      ".wasm",
-      ".ts",
-      ".tsx",
-      ".mjs",
-      ".cjs",
-      ".js",
-      ".json",
-      ".js",
-      ".jsx",
+      '.wasm',
+      '.ts',
+      '.tsx',
+      '.mjs',
+      '.cjs',
+      '.js',
+      '.json',
+      '.js',
+      '.jsx',
+      '.vue',
     ],
   },
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
         test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
-        options: {
-          presets: [
-            [
-              "@babel/preset-react",
-              {
-                runtime: "automatic",
-              },
-            ],
-            [
-              "@babel/preset-env",
-              {
-                targets: "defaults",
-              },
-            ],
-          ],
-        },
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
+        use: [stylesHandler, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        type: 'asset',
       },
     ],
   },
@@ -76,11 +64,11 @@ const config = {
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production';
 
     config.plugins.push(new MiniCssExtractPlugin());
   } else {
-    config.mode = "development";
+    config.mode = 'development';
   }
   return config;
 };
