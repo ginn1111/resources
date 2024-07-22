1ce106095227964b0b22805ccfd2e3dc5b048a8a
ginn1111 <vanthuanjw@gmail.com>
2024-07-22 17:29:45 +0700

    cli-app: finish todo app

--- cli-app/todo-app/index.ts
+++ cli-app/todo-app/index.ts
@@ -1,5 +1,8 @@
 import { Client } from "pg";
 import { program } from "commander";
+import ora from "ora";
+
+program.version("0.0.1").description("To do app with PostgreSQL");
 
 const client = new Client({
   user: "postgres",
@@ -16,54 +19,72 @@ interface Todo {
 }
 
 function getList(state: string): Promise<Todo[]> {
+  const spinner = ora("Listing...");
   return new Promise((resolve, reject) => {
     let SQL = `SELECT * FROM todo_item`;
     if (state != "all") {
       SQL += ` WHERE state='${state === "all" ? null : state}'`;
     }
+    spinner.start();
     client.query(SQL, (err, result) => {
       if (err) {
         reject(err);
+        spinner.fail(err.message);
         return;
       }
 
       resolve(result.rows);
+      spinner.succeed();
     });
   });
 }
 
 function addNew(content: string) {
+  const spinner = ora("Adding...");
   return new Promise((resolve, reject) => {
+    spinner.start();
     client.query(
       `INSERT INTO todo_item (content, state) VALUES ('${content}', 'pending')`,
       (err, result) => {
-        if (err) reject(err);
+        if (err) {
+          reject(err);
+          spinner.fail(err.message);
+        }
         resolve(result);
+        spinner.succeed("Added!");
       },
     );
   });
 }
 
 function deleteTodo(id: string) {
+  const spinner = ora("Deleting...");
   return new Promise((resolve, reject) => {
+    spinner.start();
     client.query(`DELETE from todo_item WHERE id = ${id}`, (err, result) => {
       if (err) {
         reject(err);
+        spinner.fail(err.message);
       }
       resolve(result);
+      spinner.succeed("Deleted!");
     });
   });
 }
 
 function done(id: string) {
+  const spinner = ora("Done...");
   return new Promise((resolve, reject) => {
+    spinner.start();
     client.query(
       `UPDATE todo_item SET state = 'done' WHERE id = ${id}`,
       (err, result) => {
         if (err) {
           reject(err);
+          spinner.fail(err.message);
         }
         resolve(result);
+        spinner.succeed();
       },
     );
   });
@@ -78,74 +99,27 @@ start().then(async () => {
     try {
       program
         .option("-n, --new <todo>", "Add new todo")
-        .action(async (options) => {
-          resolve(await addNew(options.new));
-        });
-
-      program
         .option("-l, --list <state>", "List todo")
-        .action(async (options) => {
-          const list = await getList(options.list);
-          console.log(list);
-          resolve(list);
-        });
+        .option("-d, --delete <id>", "Delete todo")
+        .option("--done <id>", "Done todo")
+        .parse();
 
-      program.parse();
+      const options = program.opts();
+
+      if (options.new) {
+        addNew(options.new).then(() => resolve("ok"));
+      } else if (options.list) {
+        getList(options.list).then((list) => {
+          console.log(list);
+          resolve("ok");
+        });
+      } else if (options.delete) {
+        deleteTodo(options.delete).then(() => resolve("ok"));
+      } else if (options.done) {
+        done(options.done).then(() => resolve("ok"));
+      }
     } catch (err) {
       reject(err);
     }
   }).then(() => client.end());
-
-  // const [, , action, ...args] = process.argv;
-  //
-  // switch (action) {
-  //   case "--version": {
-  //     console.log((await import("./package.json")).version);
-  //     break;
-  //   }
-  //
-  //   case "--delete": {
-  //     const id = args[0];
-  //     if (!id) {
-  //       console.error("Id do not exists!");
-  //       return;
-  //     }
-  //
-  //     await deleteTodo(id);
-  //     console.log("Performed successfully");
-  //     break;
-  //   }
-  //   case "--done": {
-  //     const id = args[0];
-  //     if (!id) {
-  //       console.error("Id do not exists!");
-  //       return;
-  //     }
-  //     await done(id);
-  //
-  //     console.log("Performed successfully");
-  //     break;
-  //   }
-  //   case "--new": {
-  //     await addNew(args[0]);
-  //
-  //     console.log("Performed successfully");
-  //     break;
-  //   }
-  //   case "--list": {
-  //     const state = args[0] ?? "all";
-  //
-  //     console.log(
-  //       (await getList(state)).map(
-  //         ({ id, content, state }) => `${id}:${content}-${state}`,
-  //       ),
-  //     );
-  //
-  //     break;
-  //   }
-  //
-  //   default:
-  //     console.warn("Action does not exist");
-  // }
 });
-
